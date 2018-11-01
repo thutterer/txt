f0711787e3d7e79f616637ea4b14c4064abcd55a
Thomas Hutterer <thutterer@suse.de>
2018-11-01 12:08:54 +0100

    throw out more stuff

--- app.js
+++ app.js
@@ -20,82 +20,16 @@ function newTxt() {
   return new_txt;
 }
 
-function removeTxt(txt, focus) {
-  // if(focus) {
-  //   var closest_txt = txt.previousElementSibling;
-  //   if(!closest_txt && txt.nextElementSibling && txt.nextElementSibling.className == 'txt') closest_txt = txt.nextElementSibling;
-  //   if(!closest_txt) {
-  //     var previous_row = txt.closest('.row').previousElementSibling;
-  //     var next_row = txt.closest('.row').nextElementSibling;
-  //     if(previous_row) closest_txt = previous_row.querySelector('.txt:last-of-type')
-  //     else closest_txt = next_row.querySelector('.txt:first-of-type');
-  //   }
-  //   closest_txt.getElementsByTagName('textarea')[0].focus();
-  // }
-
-  // if(confirm("Changes won't be saved.")) txt.remove();
-  txt.remove();
-}
-
-function appendTxtAfter(here) {
-  var this_txt = here.closest('.txt');
-  var new_txt = newTxt();
-
-  new_txt.appendAfter(this_txt);
-
-  var txt_area = new_txt.getElementsByTagName('textarea')[0]
-  txt_area.focus();  // TODO: DRY!
-
-  return new_txt;
-}
-
-function appendTxtBefore(here) {
-  var this_txt = here.closest('.txt');
-  var new_txt = newTxt();
-
-  new_txt.appendBefore(this_txt);
-
-  var txt_area = new_txt.getElementsByTagName('textarea')[0]
-  txt_area.focus();  // TODO: DRY!
-
-  return new_txt;
-}
-
 function appendTxtToRow() {
   var row = document.getElementsByClassName('row')[0];
   var new_txt = newTxt();
 
   row.appendChild(new_txt);
-  // new_txt.scrollIntoView({behavior: 'smooth'});
 
   var txt_area = new_txt.getElementsByTagName('textarea')[0]
-  txt_area.focus();  // TODO: DRY!
+  txt_area.focus();
 
   return new_txt;
-}
-
-function keyEvents(e) {
-  if(e.ctrlKey) {
-    var this_txt = document.activeElement.closest('.txt');
-    var this_row = document.activeElement.closest('.row');
-
-    if(e.shiftKey) {
-      switch (e.keyCode) {
-        case 37: // arrow-left
-          var new_txt = appendTxtBefore(this_txt);
-          break;
-        case 38: // arrow-up
-          break;
-        case 39: // arrow-right
-          var new_txt = appendTxtAfter(this_txt);
-          break;
-        case 40: // arrow-down
-          break;
-        default:
-      }
-      document.getElementById(new_txt.id).getElementsByTagName('textarea')[0].focus();
-    }
-  }
 }
 
 function changeColor(hue) {
@@ -111,6 +45,7 @@ function createColorButton(hue) {
   btn.setAttribute('onClick', 'changeColor(' + hue + ')');
   colors.appendChild(btn);
 }
+
 // https://www.w3schools.com/html/html5_draganddrop.asp
 
 function drag(ev) {
@@ -147,10 +82,4 @@ function minimizeTxt(txt) {
 function restoreTxt(txt) {
   document.getElementsByClassName('row')[0].appendChild(txt);
   txt.getElementsByClassName('title')[0].disabled = false;
-}
-
-function showFab(condition) {
-  var fab = document.getElementById('fab');
-  if(condition) fab.classList.remove('hidden');
-  else fab.classList.add('hidden');
 }
