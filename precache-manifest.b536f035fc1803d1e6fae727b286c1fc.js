31a5d1745db19a5c4cb989acd307550fd22b9ef9
Thomas Hutterer <thutterer@suse.de>
2020-09-03 12:08:14 +0200

    Updates

---
+++ precache-manifest.b536f035fc1803d1e6fae727b286c1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6779cc2839b8057494e956efa21995",
+    "url": "/txt/index.html"
+  },
+  {
+    "revision": "f7eae238f8e0622bab74",
+    "url": "/txt/static/css/main.0b0dd4d5.chunk.css"
+  },
+  {
+    "revision": "e49e393d01a8c85389fe",
+    "url": "/txt/static/js/2.d4263d0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/txt/static/js/2.d4263d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7eae238f8e0622bab74",
+    "url": "/txt/static/js/main.c3527f9f.chunk.js"
+  },
+  {
+    "revision": "7c116aa499e319e35034",
+    "url": "/txt/static/js/runtime-main.2e17075e.js"
+  },
+  {
+    "revision": "e35fa18e0fdbbbba0b649f60d6c441ce",
+    "url": "/txt/static/media/logo.e35fa18e.png"
+  }
+]);
