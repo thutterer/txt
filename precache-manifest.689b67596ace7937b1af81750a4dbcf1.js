0f1f78b894904e0f9372ab38e8828a7924baee54
Thomas Hutterer <tohu@tuta.io>
2020-11-13 12:56:39 +0100

    Updates

--- precache-manifest.689b67596ace7937b1af81750a4dbcf1.js
+++ precache-manifest.689b67596ace7937b1af81750a4dbcf1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee05398b0d6fd8619505b490790b8100",
+    "revision": "f53274fc8cd3f840a1eb14416cad17f1",
     "url": "/txt/index.html"
   },
   {
-    "revision": "049b6c98595589ca8e37",
-    "url": "/txt/static/css/main.b087b064.chunk.css"
+    "revision": "0d74a3de94e0139e4226",
+    "url": "/txt/static/css/main.f3ff1a66.chunk.css"
   },
   {
-    "revision": "faf3cfe9bdbf11b159e4",
-    "url": "/txt/static/js/2.220febd9.chunk.js"
+    "revision": "d8ae0dccd3c5415b5085",
+    "url": "/txt/static/js/2.c0855401.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/txt/static/js/2.220febd9.chunk.js.LICENSE.txt"
+    "url": "/txt/static/js/2.c0855401.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "049b6c98595589ca8e37",
-    "url": "/txt/static/js/main.bc50cea2.chunk.js"
+    "revision": "0d74a3de94e0139e4226",
+    "url": "/txt/static/js/main.d1f09ced.chunk.js"
   },
   {
     "revision": "7c116aa499e319e35034",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/txt/precache-manifest.77f775efd20cf46c5e521fc3b96a3778.js"
+  "/txt/precache-manifest.689b67596ace7937b1af81750a4dbcf1.js"
 );
 
 self.addEventListener('message', (event) => {
