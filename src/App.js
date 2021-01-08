3fc9568cfe1a9c8fa00114ad371e079e241acf83
Thomas Hutterer <tohu@tuta.io>
2021-01-08 09:41:44 +0100

    Remove HTML app header
    I never want to see this (but use the PWA) and also it saves the request fetching the image.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.png';
 import './App.css';
 import Pad from './components/Pad.js'
 
@@ -7,12 +6,6 @@ import Pad from './components/Pad.js'
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="https://github.com/thutterer/txt/">
-          <img src={logo} className="App-logo" alt="logo" />
-        </a>
-      </header>
-
       <main>
         <Pad />
       </main>
