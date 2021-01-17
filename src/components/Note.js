5580c68d41c09025df8e86fb4d2f05b6a73a8db4
Thomas Hutterer <tohu@tuta.io>
2021-01-17 17:27:50 +0100

    Integrate with the OS sharing UI with the Web Share API
    See https://web.dev/web-share/

--- src/components/Note.js
+++ src/components/Note.js
@@ -27,6 +27,16 @@ class Note extends React.Component {
     }, 50)
   }
 
+  shareText() {
+    if (navigator.share) {
+      navigator.share({
+        text: this.props.value,
+      })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    }
+  }
+
   render() {
     return (
       <div className="note" style={{ borderBottom: `4px solid ${this.props.color || 'transparent'}` }}>
@@ -63,6 +73,13 @@ class Note extends React.Component {
         <footer>
           <button
             className="icon"
+            title="Add a task"
+            onClick={ () => this.createAndEditNewTask() }>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z"/></svg>
+          </button>
+
+          <button
+            className="icon"
             title="Colorize"
             onClick={() => this.colorInput.current.click()}>
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20.71 5.63l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-3.12 3.12-1.93-1.91-1.41 1.41 1.42 1.42L3 16.25V21h4.75l8.92-8.92 1.42 1.42 1.41-1.41-1.92-1.92 3.12-3.12c.4-.4.4-1.03.01-1.42zM6.92 19L5 17.08l8.06-8.06 1.92 1.92L6.92 19z"/></svg>
@@ -75,20 +92,20 @@ class Note extends React.Component {
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg>
           </button>
 
-          <button
-            className="icon"
-            title="Add a task"
-            onClick={ () => this.createAndEditNewTask() }>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z"/></svg>
-          </button>
-
-          {(this.props.value.length === 0 && (!this.props.tasks || this.props.tasks.length === 0)) &&
+          {(this.props.value.length === 0 && (!this.props.tasks || this.props.tasks.length === 0)) ?
             <button
               title="Delete permanently"
               className="icon"
               onClick={ () => this.props.dispatch({ type: 'deleteNote', payload: { id: this.props.id } }) }>
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/></svg>
-            </button>}
+            </button> :
+            navigator.share && <button
+            title="Share (text only)"
+            className="icon"
+            onClick={ () => this.shareText() }>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
+          </button>
+          }
         </footer>
       </div>
     )
