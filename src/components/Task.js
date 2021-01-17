bc0c1fbeb29b31ff28f03cc6f9800da81ee9bc4f
Thomas Hutterer <tohu@tuta.io>
2021-01-17 08:48:49 +0100

    Add missing title to icon buttons

--- src/components/Task.js
+++ src/components/Task.js
@@ -46,7 +46,7 @@ export default function Task(props) {
       /> }
       { editMode || <span>{ props.date }</span> }
 
-      { !props.checked && <button className='icon' onClick={toggleEditMode}>
+      { !props.checked && <button className='icon' title='Toggle edit mode' onClick={toggleEditMode}>
         { editMode || <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>}
         { editMode && <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>}
       </button>}
